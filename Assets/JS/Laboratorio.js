d88647b554052fd03eb68761e9099190263af18a
Julián González <julian.gonzalez.arrascaeta.2008@gmail.com>
2026-06-20 17:21:21 -0300

    (Feat) Registrar Solicitudes de Software
    resolves #56 Se añadió una funcion que acepta detalles funcional y que guarda en localStorage

--- Assets/JS/Laboratorio.js
+++ Assets/JS/Laboratorio.js
@@ -1,4 +1,7 @@
 const form = document.getElementById("LabForm");
+const solicitudSoftware = document.getElementById("SolicitudDeSoftware");
+const detalleSoftware = document.getElementById("DetalleSoftware");
+
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -6,6 +9,8 @@ form.addEventListener('submit', function(e) {
     const solicitud = {
         id: Date.now(),
         laboratorio: document.getElementById("laboratorioSolicitud").value,
+        solicitudSoftware: document.getElementById("SolicitudDeSoftware").value,
+        detalleSoftware: document.getElementById("DetalleSoftware").value || "Sin detalle",
     }
     const solicitudGuardada = JSON.parse(localStorage.getItem('Solicitudes')) || [];
 
@@ -15,3 +20,18 @@ form.addEventListener('submit', function(e) {
     console.log("Ticket guardado:", solicitud);
     form.reset();
 });
+
+solicitudSoftware.addEventListener('change', function() {
+    if (this.value === "Si") {
+        detalleSoftware.required = true;
+        detalleSoftware.disabled = false;
+    } else {
+        detalleSoftware.required = false;
+        detalleSoftware.disabled = true;
+        detalleSoftware.value = "";
+    }
+});
+
+detalleSoftware.disabled = true;
+detalleSoftware.required = false;
+
